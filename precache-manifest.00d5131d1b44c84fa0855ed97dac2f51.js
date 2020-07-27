1ebfeec8285c1c5e32af228aa6f405261c23b3bf
ablashch <ablashch@gmail.com>
2020-07-27 13:01:50 +0300

    Updates

---
+++ precache-manifest.00d5131d1b44c84fa0855ed97dac2f51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc49bea7eb4e08248215959d1599194f",
+    "url": "/MyPortfolioApp/index.html"
+  },
+  {
+    "revision": "a08e0d74dfb7c8ce5b36",
+    "url": "/MyPortfolioApp/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "af8a69ea3b856520ba5c",
+    "url": "/MyPortfolioApp/static/css/main.40803fd2.chunk.css"
+  },
+  {
+    "revision": "a08e0d74dfb7c8ce5b36",
+    "url": "/MyPortfolioApp/static/js/2.7dbbd5d6.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/MyPortfolioApp/static/js/2.7dbbd5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8a69ea3b856520ba5c",
+    "url": "/MyPortfolioApp/static/js/main.1bd991ff.chunk.js"
+  },
+  {
+    "revision": "0b11be5884c3a2604be4",
+    "url": "/MyPortfolioApp/static/js/runtime-main.5ded051e.js"
+  }
+]);
